5a06926f686ac9d33b4b4b0915cd39ea68248662
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2025-11-03 09:44:06 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,14 @@ import streamlit as st
 from home import show_home
 from dashboard import show_dashboard
 
+st.set_page_config(page_title='CODEFARM', page_icon=':seedling:')
+
 st.sidebar.title('🔍 메뉴')
 page = st.sidebar.radio('페이지 선택', ['홈', '환경 대시보드'])
 
-if page == '🏠 홈':
+if page == '홈':
     show_home()
-elif page == '🌿 환경 대시보드':
+elif page == '환경 대시보드':
     show_dashboard()
 
 
