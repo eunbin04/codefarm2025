684d5fbbffbc04bffd11b6fd72d0e8dedb2d5e03
eunbin04 <153293377+eunbin04@users.noreply.github.com>
2025-11-05 17:34:55 +0000

    alarms

--- alarms.py
+++ alarms.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+
+def color_status(val):
+    if val == "해결됨":
+        color = "lightgreen"
+    elif val == "미해결":
+        color = "lightcoral"
+    else:
+        color = ""
+    return f"background-color: {color}"
+
+
 def show_alarms():
     st.title("🚨 알림")
 
@@ -18,22 +29,24 @@ def show_alarms():
 
     df_alarms = pd.DataFrame(alarm_data)
 
-    st.dataframe(df_alarms.style.applymap(color_status, subset=["상태"]))
+    # 상태별 필터링 셀렉트박스
+    status_filter = st.selectbox("알림 상태 선택", options=["전체", "해결됨", "미해결"])
+
+    if status_filter == "전체":
+        filtered_df = df_alarms
+    else:
+        filtered_df = df_alarms[df_alarms["상태"] == status_filter]
+
+    st.dataframe(filtered_df.style.applymap(color_status, subset=["상태"]))
 
     st.markdown("### 알림 상세")
-    selected = st.selectbox("알림 선택", df_alarms.index)
+    selected = st.selectbox("알림 선택", filtered_df.index)
     if selected is not None:
-        st.write("###", df_alarms.loc[selected, "알림 유형"])
-        st.write("시간:", df_alarms.loc[selected, "시간"])
-        st.write("상태:", df_alarms.loc[selected, "상태"])
-        st.write("설명:", df_alarms.loc[selected, "설명"])
-
-
-def color_status(val):
-    if val == "해결됨":
-        color = "lightgreen"
-    elif val == "미해결":
-        color = "lightcoral"
-    else:
-        color = ""
-    return f"background-color: {color}"
+        st.write("###", filtered_df.loc[selected, "알림 유형"])
+        st.write("시간:", filtered_df.loc[selected, "시간"])
+        st.write("상태:", filtered_df.loc[selected, "상태"])
+        st.write("설명:", filtered_df.loc[selected, "설명"])
+        if filtered_df.loc[selected, "상태"] == "미해결":
+            if st.button("해결됨으로 표시"):
+                filtered_df.at[selected, "상태"] = "해결됨"
+                st.success("알림 상태가 '해결됨'으로 업데이트되었습니다.")  
